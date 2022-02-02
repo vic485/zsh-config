81faccffb321fc08f5c91b0171e571002e061bb5
Victor Fugazzotto <victorfuga@gmail.com>
2022-02-02 18:16:08 -0500

    Adjust theme 'os icon'

--- .p10k.zsh
+++ .p10k.zsh
@@ -30,7 +30,7 @@
 
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    # os_icon               # os identifier
+    os_icon               # os identifier
     dir                     # current directory
     vcs                     # git status
     # prompt_char           # prompt symbol
@@ -178,7 +178,7 @@
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
   # Custom icon.
-  # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='🔴'
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
